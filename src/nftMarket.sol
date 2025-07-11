6104befd9a59077d7fd841b2bd0531270b4e86db
Vivsaan-Singh <vivswaan.singh@syvora.com>
2025-07-11 15:59:44 +0530

    feat: made code more modular

--- src/nftMarket.sol
+++ src/nftMarket.sol
@@ -38,43 +38,43 @@ contract Sale is Ownable {
     }
  
     function listNft721(address _nftContractAddress,address _ERC20address,uint256 _tokenID, uint256 _price) public {
-        require(Nft(_nftContractAddress).ownerOf(_tokenID)==msg.sender, NotOwner(msg.sender,Nft(_nftContractAddress).ownerOf(_tokenID)));
-        idToMarketItem[_tokenID][_nftContractAddress]=MarketItem(_tokenID,msg.sender,_nftContractAddress,_ERC20address,_price,1,true); 
-        saleExists[_tokenID][_nftContractAddress]=true;
+        require(ERC721(_nftContractAddress).ownerOf(_tokenID)==msg.sender, NotOwner(msg.sender,Nft(_nftContractAddress).ownerOf(_tokenID)));
+        _enlist(msg.sender, _nftContractAddress, _ERC20address, _tokenID, _price, 1, true);
     }
 
     function listNft1155(address _nftContractAddress,address _ERC20address,uint256 _tokenID, uint256 _price, uint256 _amount) public {
-        require(Token(_nftContractAddress).balanceOf(msg.sender, _tokenID)!=0, ErrorListing1155());
-        idToMarketItem[_tokenID][_nftContractAddress]=MarketItem(_tokenID,msg.sender,_nftContractAddress,_ERC20address,_price,_amount,false); 
-        saleExists[_tokenID][_nftContractAddress]=true;
+        require(ERC1155(_nftContractAddress).balanceOf(msg.sender, _tokenID)!=0, ErrorListing1155());
+        _enlist(msg.sender, _nftContractAddress, _ERC20address, _tokenID, _price, _amount, false);
     }
-
 
     function purchaseNft721(address nftAddress, uint256 tokenId) public payable {
         require(saleExists[tokenId][nftAddress],SaleDoesNotExists());
-        MarketItem memory listedItem = idToMarketItem[tokenId][nftAddress];
-        uint256 fees = (listedItem.price*55)/(10000);
-        uint256 sellerAmount=listedItem.price-fees;
-        saleExists[tokenId][nftAddress]=false;
-        delete (idToMarketItem[tokenId][nftAddress]);
-        if(listedItem.paymentAddress==address(0)){
-            require(msg.value>=listedItem.price, LessEther(msg.value,listedItem.price));
-            (bool sent, ) = listedItem.sellerAdderess.call{value: sellerAmount}("");
-            require(sent, EtherNotSent());
-            (bool sentback, ) = msg.sender.call{value: msg.value-listedItem.price}("");
-            require(sentback, EtherNotSentBack());
-            sellerEarnings[saleOwner]+=fees;
-        }
-        else{
-            ERC20(listedItem.paymentAddress).transferFrom(msg.sender,listedItem.sellerAdderess,sellerAmount);
-            ERC20(listedItem.paymentAddress).transferFrom(msg.sender,saleOwner,fees);
-            sellerEarnings[saleOwner]+=fees;
-        }
-        ERC721(nftAddress).transferFrom(listedItem.sellerAdderess, msg.sender, tokenId);
+        address seller = idToMarketItem[tokenId][nftAddress].sellerAdderess;
+        _startSelling(nftAddress, tokenId, 1);
+        ERC721(nftAddress).transferFrom(seller, msg.sender, tokenId);
     }
 
     function purchaseNft1155(address nftAddress, uint256 tokenId, uint256 amount) public payable {
         require(saleExists[tokenId][nftAddress],SaleDoesNotExists());
+        address seller = idToMarketItem[tokenId][nftAddress].sellerAdderess;
+        _startSelling(nftAddress, tokenId, amount);
+        ERC1155(nftAddress).safeTransferFrom(seller, msg.sender, tokenId, amount, "");
+    }
+
+    function getSeller(address nftAddress, uint256 tokenId) public view returns(address) {
+        return idToMarketItem[tokenId][nftAddress].sellerAdderess;
+    }
+
+    function getSaleAddress() external view returns(address){
+        return address(this);
+    }
+
+    function _enlist(address seller, address _nftContractAddress,address _ERC20address,uint256 _tokenID, uint256 _price, uint256 _amount, bool isERC721) private{
+        idToMarketItem[_tokenID][_nftContractAddress]=MarketItem(_tokenID, seller, _nftContractAddress, _ERC20address, _price,_amount, isERC721); 
+        saleExists[_tokenID][_nftContractAddress]=true;
+    }
+
+    function _startSelling(address nftAddress, uint256 tokenId, uint256 amount) private {
         MarketItem memory listedItem = idToMarketItem[tokenId][nftAddress];
         require(amount<=listedItem.amount,NotEnoughTokens(amount,listedItem.amount));
         uint256 price = listedItem.price*amount;
@@ -98,15 +98,6 @@ contract Sale is Ownable {
             ERC20(listedItem.paymentAddress).transferFrom(msg.sender,saleOwner,fees);
             sellerEarnings[saleOwner]+=fees;
         }
-        ERC1155(nftAddress).safeTransferFrom(listedItem.sellerAdderess, msg.sender, tokenId, amount, "");
-    }
-
-    function getSeller(address nftAddress, uint256 tokenId) public view returns(address) {
-        return idToMarketItem[tokenId][nftAddress].sellerAdderess;
-    }
-
-    function getSaleAddress() external view returns(address){
-        return address(this);
     }
 
 }
